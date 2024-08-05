6f5d82c8f2913e5465bb82bc713e46a914f9d2a7
prajwalhaniya <prajwalhaniya@gmail.com>
2024-08-05 23:14:27 +0530

    c++ practice

---
+++ c++/package/practice/2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+enum Color { Red, Green, Blue };
+enum Month { 
+    jan=1, feb, mar, april, may, june, july, august
+};
+
+int main() {
+    Color mycolor = Red;
+    Month m = Month::feb;
+    
+    cout<<m<<endl;
+    
+    switch(mycolor) {
+        case Red:
+            cout<<"color is red"<<endl;
+            break;
+        case Green:
+            cout<<"color is green"<<endl;
+            break;
+        default:
+            cout<<"no color"<<endl;   
+    }
+    return 0;
+}
